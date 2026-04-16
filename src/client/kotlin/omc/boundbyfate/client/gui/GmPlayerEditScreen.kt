8bff43ee701821aaccb0c500b26d551d8dd78066
0ldxman <0ldxman@users.noreply.github.com>
2026-04-16 03:21:44 +0300

    fix: remove duplicate InventoryScreen import

--- src/client/kotlin/omc/boundbyfate/client/gui/GmPlayerEditScreen.kt
+++ src/client/kotlin/omc/boundbyfate/client/gui/GmPlayerEditScreen.kt
@@ -6,7 +6,6 @@ import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.screen.ingame.InventoryScreen
-import net.minecraft.client.gui.screen.ingame.InventoryScreen
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import omc.boundbyfate.client.state.ClientGmRegistry
