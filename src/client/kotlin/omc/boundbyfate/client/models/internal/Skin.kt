104ee3a2dc28b2e78c776e64544c2334bd2ad72e
0ldxman <0ldxman@users.noreply.github.com>
2026-05-09 00:46:37 +0300

    Some refactoring improvements

--- src/client/kotlin/omc/boundbyfate/client/models/internal/Skin.kt
+++ src/client/kotlin/omc/boundbyfate/client/models/internal/Skin.kt
@@ -14,20 +14,12 @@ class Skin(
     private val logger = LoggerFactory.getLogger(Skin::class.java)
 
     fun compute(globalRoot: Mat4f, jointGetter: Map<Int, RuntimeNode>): Array<Mat4f> {
-        val inverseRoot = MutableMat4f(globalRoot)
-        val inverted = inverseRoot.invert()
-
-        if (debugCount < 3) {
-            logger.info("[Skin] frame=$debugCount inverseRoot inverted=$inverted det=${inverseRoot.determinant()}")
-            logger.info("[Skin]   globalRoot: t=(${globalRoot.m03},${globalRoot.m13},${globalRoot.m23})")
-            logger.info("[Skin]   inverseRoot: t=(${inverseRoot.m03},${inverseRoot.m13},${inverseRoot.m23})")
-        }
-
         for ((i, id) in jointsIds.withIndex()) {
             val jointGlobalMatrix = MutableMat4f(jointGetter[id]!!.globalMatrix)
             val bindMatrix = MutableMat4f(inverseBindMatrices[i])
+            // GLTF skinning formula: skinMatrix = globalJoint * inverseBindMatrix
             val skinMatrix = MutableMat4f(jointGlobalMatrix).mul(bindMatrix)
-            cache[i] = MutableMat4f(inverseRoot).mul(skinMatrix).transpose()
+            cache[i] = skinMatrix.transpose()
         }
 
         if (debugCount < 3) {
@@ -35,21 +27,17 @@ class Skin(
             if (legIdx >= 0) {
                 val jNode = jointGetter[19]
                 val gm = jNode?.globalMatrix
-                val parentNode = jNode?.parent
-                val parentGm = (parentNode as? omc.boundbyfate.client.models.internal.v2.RuntimeNode)?.globalMatrix
                 
                 // Вычисляем skinMatrix до транспозиции для правильного лога
                 val jointGlobalMatrix = MutableMat4f(jointGetter[19]!!.globalMatrix)
                 val bindMatrix = MutableMat4f(inverseBindMatrices[legIdx])
                 val skinMatrix = MutableMat4f(jointGlobalMatrix).mul(bindMatrix)
-                val finalMatrix = MutableMat4f(inverseRoot).mul(skinMatrix)
                 
                 // translation в kool Mat4f хранится в m03, m13, m23 (column-major)
                 logger.info("[Skin] frame=$debugCount joint=19(LeftLeg)")
                 logger.info("[Skin]   globalMatrix[19]: t=(${gm?.m03},${gm?.m13},${gm?.m23})")
                 logger.info("[Skin]   inverseBindMatrix[19]: t=(${inverseBindMatrices[legIdx].m03},${inverseBindMatrices[legIdx].m13},${inverseBindMatrices[legIdx].m23})")
                 logger.info("[Skin]   skinMatrix (joint*bind): t=(${skinMatrix.m03},${skinMatrix.m13},${skinMatrix.m23})")
-                logger.info("[Skin]   finalMatrix (invRoot*skinMatrix): t=(${finalMatrix.m03},${finalMatrix.m13},${finalMatrix.m23})")
             }
             debugCount++
         }
